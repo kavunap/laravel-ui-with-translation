7f18b3484f3cd65642c450ea34d7a01fed21ccc0
kavunap <kavunap>
2022-01-15 12:32:21 +0200

    confirmable user registration

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## About this
+
+This is the file done for learning purpose and it is only used in this context.
+Laravel user authentication with email verification and  language translation.
+
+This file has the folowing
+
+- The use of laravel/ui package
+- User registration verification using email
+- Language translation of laravel/ui package
+
+
+
+## Packages used
+
+- [laravel-mail-viewer](https://github.com/MasterRO94/laravel-mail-viewer)
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
+
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
+
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Auth::routes(['verify' => true]);
